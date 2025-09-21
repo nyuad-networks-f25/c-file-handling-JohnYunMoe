4da4cc723d7bd5adb4ace652238dc1076f201632
JohnMoe <jy4017@nyu.edu>
2025-09-21 19:47:29 +0000

    added answers.txt and changed main.c to reflect question 1

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
-int main() { return 0; }
+#include <stdio.h>
+
+int main(int argc, char *argv[]) 
+{ 
+    if (argc < 2)
+    {
+        printf("%s Needs a filename as argument\n", argv[0]);
+        return 1;
+    }
+    return 0; 
+}
