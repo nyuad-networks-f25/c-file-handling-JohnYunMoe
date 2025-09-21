d37b0377ff64c997b791b04b9178efecf41048f2
JohnMoe <jy4017@nyu.edu>
2025-09-21 20:43:02 +0000

    added back data.bin after removing for testing

--- main.c
+++ main.c
@@ -13,6 +13,22 @@ int main(int argc, char *argv[])
         printf("Could not open data.bin for reading.\n");
         return 1;
     }
+
+    // Skip the first 40 bytes (preamble)
+    fseek(file, 40, SEEK_SET);
+
+    // Read the first record: Name[7B], id[2B], grade[1B]
+    char name[7];
+    unsigned char id[2];
+    unsigned char grade;
+
+    fread(name, 1, 7, file); // skip name
+    fread(id, 1, 2, file);   // read id
+    fread(&grade, 1, 1, file); // read grade
+
+    // Print id and grade
+    printf("%u %u\n", (id[0] << 8) | id[1], grade);
+
     fclose(file);
-    return 0; 
+    return 0;
 }
